fd2717d21a1fc1b39057956d8b7558f74a35a4de
Colby Pearce <crpearce892@gmail.com>
2022-06-08 20:05:07 -0600

    add arguements to player and game class

---
+++ player.js
@@ -0,0 +1,12 @@
+var Game = require('./game');
+
+class Player {
+  constructor(name) {
+    this.name = name
+    this.token = 🥷🏻 || 💻
+    this.wins = 0
+    this.takeTurn = true
+  }
+}
+
+module.exports = Player;
